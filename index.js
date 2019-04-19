eb65eef59238feb4091f41452556ad893abdccdc
Christopher Wilson <chris.sa.wilson@gmail.com>
2019-04-19 15:20:33 -0400

    Updated main index.js file

--- index.js
+++ index.js
@@ -1,33 +1,35 @@
-// Client ID: qfi2ddm26kctaiej2a9f878wdgl7cv
+// Constants
+const RANDOM = null;
+const STREAM = 1;
+const CHANNEL = 0;
+const KEYCODE_ENTER = 13;
+const KEYCODE_ESC = 27;
+const BLANK_IMG = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8z8BQDwAEhQGAhKmMIQAAAABJRU5ErkJggg==";
 
-var searchType = "streams";
-var currentlyPlaying = null;
-var player = null;
-var lastSearch = null; // Prevents duplicate search API calls
-var lastSearchType = null; // Prevents duplicate search API calls
+// Global Variables
+var searchType = STREAM; 
+var currentlyPlaying = null; // Object for currently playing channel
+var player = null; // Twitch video player object
 var numPinnedChannels = 0;
-
-// Configurable
-var NUM_RESULTS = 20; // The number of search results to display
-var UPDATE_FREQUENCY = 300; // Minimum delay between api calls while typing in search box
-var MAX_PIN_COUNT = 1;
-var COOKIE_EXPIRATION_DAYS = 7; // Number of days until cookies expire
-
-// Final Global Variables (Do not edit)
-var RANDOM = null;
-var STREAM = 1;
-var CHANNEL = 0;
-var KEYCODE_ENTER = 13;
-var KEYCODE_ESC = 27;
+var searchHistory = new SearchList();
 var updateTimer; // Limits API calls while typing
-var BLANK_IMG =
-  "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8z8BQDwAEhQGAhKmMIQAAAABJRU5ErkJggg==";
-
 var pinnedChannels = new Array(MAX_PIN_COUNT);
 
-// Booleans
+// Configurable Variables
+var NUM_RESULTS = 20; // The max number of search results to display (TODO: This is not implemented yet)
+var UPDATE_FREQUENCY = 300; // Minimum delay between api calls while typing in search box
+var MAX_PIN_COUNT = 8; // Maximum number of pinned channels (TODO: currently hard-coded in class definition)
+var COOKIE_EXPIRATION_DAYS = 2; // Number of days until cookies expire
+var SEARCHES_TO_CACHE = 10; // Cache this many past searches
+
+// Boolean Functions (TODO: Make these methods of an object)
 function searchboxIsEmpty() { return $("#searchbox").val() === ""; }
-function playing() { return currentlyPlaying !== null; }
+function streamIsPlaying() { return currentlyPlaying !== null; }
+
+// Ready function
+$(function() {
+  processCookies();
+});
 
 // Cookies
 function createCookie(name,value,days) {
@@ -43,7 +45,7 @@ function createCookie(name,value,days) {
 function readCookie(name) {
 	var nameEQ = name + "=";
 	var ca = document.cookie.split(';');
-	for(var i=0;i < ca.length;i++) {
+	for(let i=0;i < ca.length;i++) {
 		var c = ca[i];
 		while (c.charAt(0)==' ') c = c.substring(1,c.length);
 		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
@@ -55,61 +57,55 @@ function eraseCookie(name) {
 	createCookie(name,"",-1);
 }
 
-// Ready Function
-$(function() {
+function processCookies() {
+  var channelObjectsFromCookies = [];
   var hasCookies = false;
-  var channelsToPin = [];
-  for(var i=0;i<MAX_PIN_COUNT; i++) {
-    var streamName = readCookie(i);
-    if(streamName === -1) { continue; }
+  for(let i=0;i<MAX_PIN_COUNT; i++) {
+    var channelName = readCookie(i);
+    if(channelName === -1) continue; // if cookie data === -1, then cookie was erased, skip it.
     hasCookies = true;
-    console.log(streamName);
-    var url = getChannelURL(streamName);
-    console.log("URL" + url);
-    channelsToPin.push($.getJSON(url));
+    console.log("READ COOKIE: " + channelName);
+    var url = getChannelURL(channelName);
+    console.log("A: " + url);
+    channelObjectsFromCookies.push($.getJSON(url));
   }
-  if(hasCookies) {
-   $.when.apply($, channelsToPin).then(function() {
-     $("#loading-pins-icon").hide();
-     $("#pinned-channels-section").show();
-     console.log(arguments.length);
-     console.log(arguments[0].length);
-     if(arguments[0].constructor === Array) {
-      for(var j=0;j<arguments.length;j++) {
-        pinChannel(arguments[j][0]);
-      }
-     }
-     else {
+  if(!hasCookies) {
+    displayPinnedChannels();
+    return -1;
+  }
+  $.when.apply($, channelObjectsFromCookies).then(function() {
+    displayPinnedChannels();
+     if(arguments[0].constructor !== Array) {
        pinChannel(arguments[0]);
+       return 1;
+     }
+     for(let i=0;i<arguments.length;i++) {
+       pinChannel(arguments[i][0]);
      }
    });
-  }
-  else {
-    $("#loading-pins-icon").hide();
-    $("#pinned-channels-section").show();
-  }
-});
+}
 
+function displayPinnedChannels() {
+  $("#loading-pins-icon").hide();
+  $("#pinned-channels-section").show();
+}
+
+// Event Listeners
 $("#searchbychannel").click(function() {
-  if(searchType == "channels") {
-    $("#searchbox").focus();
-    return;
-  }
+  $("#searchbox").focus();
+  if(searchType === CHANNEL) return;
   clearResults();
-  searchType = "channels";
-  console.log(searchType + "AAAA");
+  searchType = CHANNEL;
   $("#searchbychannel").addClass("selected");
   $("#searchbystream").removeClass("selected");
   update();
   $("#searchbox").focus();
 });
 $("#searchbystream").click(function() {
-  if(searchType == "streams") {
-    $("#searchbox").focus();
-    return;
-  }
+  $("#searchbox").focus();
+  if(searchType === STREAM) return;
   clearResults();
-  searchType = "streams";
+  searchType = STREAM;
   $("#searchbychannel").removeClass("selected");
   $("#searchbystream").addClass("selected");
   update();
@@ -136,11 +132,6 @@ $("#searchbox").blur(function() {
   if(!$("#resultsbox").is(":focus")) {
     $("#resultsbox").hide();
   }
-});
-
-Sortable.create(document.getElementById("pinned-channels"), { animation: 150 });
-$('form').submit(function(event){
-  event.preventDefault();
 });
 $("#collapse-bar").on('shown.bs.collapse', function() {
   $("#searchbox").focus();
@@ -170,135 +161,68 @@ $("#searchbutton").blur(function() {
   }
 });
 
+Sortable.create(document.getElementById("pinned-channels"));
+$('form').submit(function(evt){
+  evt.preventDefault();
+});
+
 function getPinIndex() {
-  for(var i=0;i<MAX_PIN_COUNT;i++) {
+  for(let i=0;i<MAX_PIN_COUNT;i++) {
     if(!pinnedChannels[i]) { return i; }
   }
   return -1; // No more pin slots available
 }
 
-function update(instant) {
-  if (searchboxIsEmpty()) {
+function update() {
+  if(searchboxIsEmpty()) {
     clearTimeout(updateTimer);
-    console.log("EMPTY");
     clearResults();
-    lastSearch = "";
     return;
   }
   clearTimeout(updateTimer);
-  updateTimer = setTimeout(search, UPDATE_FREQUENCY);
+  var newSearch = generateSearchObject();
+  var cachedSearchIndex = searchHistory.indexOf(newSearch);
+  if(cachedSearchIndex !== -1)
+    search(cachedSearchIndex);
+  else
+    updateTimer = setTimeout(search, UPDATE_FREQUENCY, cachedSearchIndex);
+}
+
+function generateSearchObject() {
+  return new Search(searchType, $("#searchbox").val());
 }
 
 function clearResults() {
-  lastSearch = "";
   $("#resultsbox").html("");
 }
 
 function loadingIcon() {
-  console.log("LOADING ICON");
   $("#searchbutton-icon").removeClass();
   $("#searchbutton-icon").addClass("fa fa-spinner fa-pulse");
   $('#searchbutton-icon').css('transform', 'none'); // fix for IE bug
 }
 
 function searchIcon() {
-  console.log("SEARCH ICON");
   $("#searchbutton-icon").removeClass();
   $("#searchbutton-icon").addClass("fa fa-search");
   $('#searchbutton-icon').css('transform', 'none'); // fix for IE bug
 }
 
-function search() {
-  var searchTerms = $("#searchbox").val();
-  console.log("LAST SEARCH: " + lastSearch);
-  console.log("LAST SEARCH TYPE: " + lastSearchType);
-  if(searchTerms == lastSearch && searchType == lastSearchType) {
-    console.log("duplicate search, no api call");
-    $("#resultsbox").show();
+function search(cachedSearchIndex) {
+  if(cachedSearchIndex !== -1) {
+    var cachedSearch = searchHistory.get(cachedSearchIndex);
+    $("#resultsbox").html(cachedSearch.generateResultsHTML());
+    cachedSearch.attachActionListeners();
     return;
   }
-  console.log("SEARCH EXECUTED: '" + searchTerms + "'");
+  var newSearch = generateSearchObject();
   loadingIcon();
-  var url = getSearchURL(searchTerms);
-  $.getJSON(url, function(data) {
-    console.log("API CALLED: " + url);
-    var results;
-    if(searchType === "streams") {
-      results = data.streams;
-    }
-    else {
-      results = data.channels;
-    }
-    displaySearchResults(results);
+  newSearch.executeSearch().then(function() {
+    $("#resultsbox").html(newSearch.generateResultsHTML());
+    newSearch.attachActionListeners();
+    searchHistory.add(newSearch);
     searchIcon();
-    lastSearch = searchTerms;
-    lastSearchType = searchType;
   });
-  
-}
-
-function displaySearchResults(results) {
-  console.log("CLEARING RESULTS");
-  clearResults();
-  if(searchType === "streams") {
-    for (var i = 0; i < results.length; i++) {
-      displayStreamResult(results[i], i);
-    }
-    return;
-  }
-  for (var i = 0; i < results.length; i++) {
-    displayChannelResult(results[i], i);
-  }
-}
-
-function displayStreamResult(result, index) {
-  var newResult = "";
-  newResult += '<div id="result' + index + '" class="result online" style="color: white;">';
-  if(!result.channel.logo) {
-    newResult += '<img src="' + BLANK_IMG + '">';
-  }
-  else {
-    newResult += '<img src="' + result.channel.logo + '">';
-  }
-  newResult += '<span class="result-title">';
-  newResult += result.channel.display_name;
-  newResult += "</span>";
-  newResult += "<p>" + result.viewers + " Viewers | " + result.game + "</p>";
-  newResult += "<p>" + result.channel.status + "</p>";
-  newResult += "</div>";
-  $("#resultsbox").append(newResult);
-  $("#result" + index).mousedown(function() {
-    console.log("RESULT " + index + " CLICKED!");
-    $("#collapse-bar").collapse("hide");
-    toggleStream(result.channel);
-  });
-}
-function displayChannelResult(result, index) {
-  var newResult = "";
-  newResult += '<div id="result' + index + '" class="result online" style="color: white;">';
-  if(!result.logo) {
-    newResult += '<img src="' + BLANK_IMG + '">';
-  }
-  else {
-    newResult += '<img src="' + result.logo + '">';
-  }
-  newResult += '<span class="result-title">';
-  newResult += result.display_name;
-  newResult += "</span>";
-  newResult += "<p>" + result.status + "</p>";
-  newResult += "</div>";
-  $("#resultsbox").append(newResult);
-  $("#result" + index).mousedown(function() {
-    console.log("RESULT " + index + " CLICKED!");
-    $("#collapse-bar").collapse("hide");
-    toggleStream(result);
-  });
-}
-
-function getSearchURL(searchTerms) {
-  var firstPart = "https://api.twitch.tv/kraken/search/";
-  var lastPart = "&client_id=qfi2ddm26kctaiej2a9f878wdgl7cv";
-  return firstPart + searchType + "?q=" + searchTerms + lastPart;
 }
 
 function getChannelURL(channelName) {
@@ -309,7 +233,6 @@ function getChannelURL(channelName) {
 
 function toggleStream(channel) {
   if (channel === currentlyPlaying) {
-    console.log("TRUE");
     hidePlayer();
     return;
   }
@@ -371,7 +294,7 @@ function pinChannel(channel) {
   }
   result += '<div class="card-block">';
   result += '<h4 class="card-title">' + channel.display_name + '</h4>';
-  result += '<p class="card-status online">Now Streaming</p>';
+  result += channel.isLive ? '<p class="card-status online">Now Streaming</p>' : '<p class="card-status offline">Offline</p>'
   result += '<p class="card-game">' + channel.game + '</p>';
   result += '</div></div>';
   $("#pinned-channels").append(result);
@@ -391,7 +314,7 @@ function pinChannel(channel) {
 function unpinChannel(pinIndex, channelName) {
   console.log("unpinning channel");
   $("#stream" + pinIndex).remove();
-  if(playing() && currentlyPlaying.name === channelName) {
+  if(streamIsPlaying() && currentlyPlaying.name === channelName) {
     $("#pin-channel-text").show();
   }
   console.log(numPinnedChannels);
@@ -406,7 +329,7 @@ function unpinChannel(pinIndex, channelName) {
     $("#remove-pin-" + i).click(function(e) {
       e.stopPropagation();
       console.log("removing channel " + pinnedChannels[i] + " at index " + i);
-    console.log(pinnedChannels);
+      console.log(pinnedChannels);
       unpinChannel(i, pinnedChannels[i]);
     });
     eraseCookie(i);
